e3cefaf4ff5084031ae47f35ce24f6ce38c40a5b
lokeshh <lokeshhsharma@gmail.com>
2016-09-25 07:08:08 +0000

    add button for download

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'song#search', as: :song
+  post 'song' => 'song#download'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
