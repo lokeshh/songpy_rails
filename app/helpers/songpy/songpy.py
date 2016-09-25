a9b8d4aa472f8ecd8220b0925d9127f3bea75e00
lokeshh <lokeshhsharma@gmail.com>
2016-09-25 10:21:28 +0000

    add songpy again

---
+++ app/helpers/songpy/songpy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+
+from google import search
+from os import system
+import sys
+
+sys.argv.pop(0)
+if sys.argv[0] == '-v':
+	video = True
+	sys.argv.pop(0)
+else:
+	video = False
+
+song_name = ' '.join(sys.argv)
+result = search(song_name).next()
+if video:
+	system('youtube-dl -s -g ' + result)
+else:
+	system('youtube-dl -f bestaudio -s -g ' + result)
+
+
